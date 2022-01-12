7ea6f4a8ca242e70bf76a41abb3f381aa61fa29b
jaeyunpark-ux <63354053+jaeyunpark-ux@users.noreply.github.com>
2022-01-12 17:25:28 +0900

    Add files via upload

---
+++ demo/src/test/java/com/example/inflearn/demo/DemoInflearnJypApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inflearn.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoInflearnJypApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
